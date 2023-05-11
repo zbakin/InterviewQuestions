6af9414cc2da84207fd26e6e0c536755002f608c
zbakin <50952098+zbakin@users.noreply.github.com>
2023-05-11 20:48:36 +0100

    improve slightly list_of_lists.py

--- list_of_lists.py
+++ list_of_lists.py
@@ -5,13 +5,9 @@
 # f(3) returns [[1], [1,2], [1,2,3]]
 
 def f(n):
-    """
-    Returns a list of n lists, where the ith list contains the numbers from 1 to i inclusive.
-    """
-    result = []
+    final_list = []
+    curr_list = []
     for i in range(1, n+1):
-        result.append(list(range(1, i+1)))
-    return result
-
-
-print(f(10))
+        curr_list.append(i) # created a sublist
+        final_list.append(curr_list[:]) # appends sublist
+    return final_list
